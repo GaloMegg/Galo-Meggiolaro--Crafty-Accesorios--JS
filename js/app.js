ebed6fa78aaf481da59c248c3727a2f3720ed282
WtsCrackin <galomeggiolarobul@gmail.com>
2021-10-26 04:02:51 -0300

    refactorign jquery

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
-//Se designan los botones como disparadores de eventos si existen los botones en la pagina
-const buyButton = document.querySelector(".buy__Button");
-if (buyButton) {
-  //La funcion controladora se ve cndicionada a la clase del boton en la que designa el objeto a comprar
-  buyButton.addEventListener("click", CartAdding);
-}
-//Se designa el Contador visual del carrito, y se designa la cantidad de elementos a comprar
-const cartCounter = document.querySelector(".cartCountingItems");
-const quantitySelectSelector = document.querySelector(".buy__Button--quantity");
-let quantitySelect;
-if (quantitySelectSelector) {
-  //La funcion controladora añade cantidad al carrito visual, y modifica la cantidad para que se vea reflejada en el dom
-  quantitySelectSelector.addEventListener("change", QuantityAdding);
-}
 //Se establece el carrito para el dom y el carrito para el carrito visual
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let literalCart = JSON.parse(localStorage.getItem("literalCart")) || [];
-//Al carrito visual de le da la cantidad de elementos
-if (cartCounter) {
-  cartCounter.textContent = literalCart.length;
+
+$(".buy__Button").on("click", CartAdding);
+$(".buy__Button--quantity").on("change", quantityValue);
+function CartAdding() {
+  if ($(".buy__Button").hasClass("aro")) {
+    PushElements(aro);
+  } else if ($(".buy__Button").hasClass("anillo")) {
+    PushElements(anillo);
+  } else if ($(".buy__Button").hasClass("collar")) {
+    PushElements(collar);
+  } else if ($(".buy__Button").hasClass("cenicero")) {
+    PushElements(cenicero);
+  }
+}
+let quantity;
+function quantityValue() {
+  quantity = $(".buy__Button--quantity option:selected").val();
+}
+function PushElements(element) {
+  for (let i = 0; i < quantity; i++) {
+    let exist = cart.some((product) => product.id === element.id);
+    if (exist) {
+      element.cantidad++;
+    } else {
+      cart.push(element);
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
 }
