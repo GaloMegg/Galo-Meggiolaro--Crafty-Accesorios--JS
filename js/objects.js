5c463340f4b3c86c07a474b0931c0a80f2f627be
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-03 09:58:49 -0300

    commenting

--- js/objects.js
+++ js/objects.js
@@ -1,20 +1,19 @@
+//Se solicita a la api los valores de las monedas en base al dolar.
 $.ajax({
   url: "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json",
   success: function (data) {
+    //Se cambia el rpecio de los objetos para ajustarlos a la inflacion
     aro.precio = Math.ceil(data.usd.ars * 4);
     anillo.precio = Math.ceil(data.usd.ars * 5);
     collar.precio = Math.ceil(data.usd.ars * 8);
     cenicero.precio = Math.ceil(data.usd.ars * 12);
-    
+    //Se modifica el texto actualizando los precios automaticamente en cada html de los productos
     $(".ashtrays__price").text(`$${cenicero.precio}`);
     $(".rings__price").text(`$${anillo.precio}`);
     $(".earrings__price").text(`$${aro.precio}`);
     $(".necklace__price").text(`$${collar.precio}`);
-
-
   },
 });
-
 
 const aro = {
   nombre: "aro",
