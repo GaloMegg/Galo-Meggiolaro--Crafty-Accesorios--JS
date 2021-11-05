d277884e35e53c7078980ce1e5cceb745297d3ef
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-05 08:46:17 -0300

    sending

--- js/form.js
+++ js/form.js
@@ -1,20 +1,19 @@
-
-$(function(){
-    $(".submitButton").submit(function(e){
-        e.preventDefault();
-        let href = $(this).attr("action");
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: href,
-            data: $(this).serialize(),
-            success: function(response){
-                if(response.status == "success"){
-                    alert("We received your submission, thank you!");
-                }else{
-                    alert("An error occured: " + response.message);
-                }
-            }
-        });
+$(function () {
+  $(".submitButton").submit(function (e) {
+    e.preventDefault();
+    let href = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: href,
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.status == "success") {
+          $(".fieldSet").trigger("reset");
+        } else {
+          alert("An error occured: " + response.message);
+        }
+      },
     });
+  });
 });
