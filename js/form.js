06425f8a0fe9f0a321f3e98aa9c02928a0b5b56a
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-05 23:37:54 -0300

    reset button form

--- js/form.js
+++ js/form.js
@@ -9,6 +9,7 @@ $(function () {
       data: $(this).serialize(),
       success: function (response) {
         if (response.status == "success") {
+            console.log (response)
           $(".fieldSet").trigger("reset");
         } else {
           alert("An error occured: " + response.message);
