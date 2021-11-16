377a453d97338f358ba99c3c817d76814d60d2d0
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-16 00:55:55 -0300

    form modification

--- js/form.js
+++ js/form.js
@@ -1,22 +1,27 @@
-$(function () {
-  $(".submitButton").submit(function (e) {
-    e.preventDefault();
-    let href = $(this).attr("action");
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      url: href,
-      data: $(this).serialize(),
-      success: function (response) {
-        if (response.status == "success") {
-            console.log (response)
-          $(".fieldSet").trigger("reset");
-        } else {
-          alert("An error occured: " + response.message);
-        }
-      },
-    });
-  });
-});
+// $(function () {
+//   $(".submitButton").submit(function (e) {
+//     e.preventDefault();
+//     let href = $(this).attr("action");
+//     $.ajax({
+//       type: "POST",
+//       dataType: "json",
+//       url: href,
+//       data: $(this).serialize(),
+//       success: function (response) {
+//         if (response.status == "success") {
+//             console.log (response)
+//           $(".fieldSet").trigger("reset");
+//         } else {
+//           alert("An error occured: " + response.message);
+//         }
+//       },
+//     });
+//   });
+// });
 
 //Shipping
+$.ajax({
+  method: GET, 
+  url: "https://formsubmit.co/api/get-apikey/galomeggiolarobul@gmail.com",
+}
+)
