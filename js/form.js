be966cb290613179b71f3f5977a0b81da0fd02fa
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-17 01:46:04 -0300

    no ajax

--- js/form.js
+++ js/form.js
@@ -8,18 +8,18 @@
 // let whatsapp = ;
 // let proyect = ;
 
-$.ajax({
-  url: "https://formsubmit.co/ajax/galomeggiolarobul@gmail.com",
-  method: "POST",
-  data: {
-    name: $("#name").val(),
-    secondName: $("#secondName").val(),
-    phoneNumber: $("#phoneNumber").val(),
-    email: $("#email").val(),
-    telOrEmail: [$("#emailradio").val(), $("#teleradio").val()],
-    gender: $("#gender").val(),
-    whatsapp: $("#whatsappsvg").val(),
-    message: $("#proyecto").val(),
-  },
-  dataType: "json",
-});
+// $.ajax({
+//   url: "https://formsubmit.co/ajax/galomeggiolarobul@gmail.com",
+//   method: "POST",
+//   data: {
+//     name: $("#name").val(),
+//     secondName: $("#secondName").val(),
+//     phoneNumber: $("#phoneNumber").val(),
+//     email: $("#email").val(),
+//     telOrEmail: [$("#emailradio").val(), $("#teleradio").val()],
+//     gender: $("#gender").val(),
+//     whatsapp: $("#whatsappsvg").val(),
+//     message: $("#proyecto").val(),
+//   },
+//   dataType: "json",
+// });
