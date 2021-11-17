ee13ce32e3e1133705b2d722acfc0831dc75ec4a
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-17 02:06:38 -0300

    form end

--- js/form.js
+++ js/form.js
@@ -17,3 +17,23 @@ $(function(){
       });
   });
 });
+
+$(function(){
+  $("#dateForm").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: href,
+          data: $(this).serialize(),
+          success: function(response){
+              if(response.status == "success"){
+                  alert("We received your submission, thank you!");
+              }else{
+                  alert("An error occured: " + response.message);
+              }
+          }
+      });
+  });
+});
