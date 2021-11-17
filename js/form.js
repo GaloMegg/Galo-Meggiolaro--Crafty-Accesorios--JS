8338901daa120e2cc0291eb9a7b9f78af37a9138
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-17 02:46:05 -0300

    reseting

--- js/form.js
+++ js/form.js
@@ -1,59 +1,68 @@
-$(function(){
-  $("#contactForm").submit(function(e){
-      e.preventDefault();
-      var href = $(this).attr("action");
-      $.ajax({
-          type: "POST",
-          dataType: "json",
-          url: href,
-          data: $(this).serialize(),
-          success: function(response){
-              if(response.status == "success"){
-                  alert("Genial, recibimos tus datos. Dentro de unas horas nos comunicaremos contigo.");
-              }else{
-                  alert("Uy, ¡algo malo paso! Intenta en otro momento..." + response.message);
-              }
-          }
-      });
+$(function () {
+  $("#contactForm").submit(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: href,
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.status == "success") {
+          alert(
+            "Genial, recibimos tus datos. Dentro de unas horas nos comunicaremos contigo."
+          );
+        } else {
+          alert(
+            "Uy, ¡algo malo paso! Intenta en otro momento..." + response.message
+          );
+        }
+      },
+    });
+    $("#contactForm").trigger("reset");
   });
 });
 
-$(function(){
-  $("#dateForm").submit(function(e){
-      e.preventDefault();
-      var href = $(this).attr("action");
-      $.ajax({
-          type: "POST",
-          dataType: "json",
-          url: href,
-          data: $(this).serialize(),
-          success: function(response){
-              if(response.status == "success"){
-                  alert("¡Te esperamos!");
-              }else{
-                  alert("Uy, ¡algo malo paso! Intenta en otro momento..." + response.message);
-              }
-          }
-      });
+$(function () {
+  $("#dateForm").submit(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: href,
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.status == "success") {
+          alert("¡Te esperamos!");
+        } else {
+          alert(
+            "Uy, ¡algo malo paso! Intenta en otro momento..." + response.message
+          );
+        }
+      },
+    });
   });
 });
 
-$(function(){
-  $("#letUsContact").submit(function(e){
-      e.preventDefault();
-      var href = $(this).attr("action");
-      $.ajax({
-          type: "POST",
-          dataType: "json",
-          url: href,
-          data: $(this).serialize(),
-          success: function(response){
-              if(response.status == "success"){
-                  alert("¡Te esperamos!");
-              }else{
-                  alert("Uy, ¡algo malo paso! Intenta en otro momento..." + response.message);
-              }
-          }
-      });
+$(function () {
+  $("#letUsContact").submit(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: href,
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.status == "success") {
+          alert("¡Te esperamos!");
+        } else {
+          alert(
+            "Uy, ¡algo malo paso! Intenta en otro momento..." + response.message
+          );
+        }
+      },
+    });
   });
 });
