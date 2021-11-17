04b3ecb20a1339e6e03467c26d5adce389db9133
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-16 23:38:06 -0300

    form send

--- js/form.js
+++ js/form.js
@@ -1,16 +1,28 @@
+let nameContact = $("#name").val();
+let secondNameContact = $("#secondName").val();
+let phoneNumberContact = $("#phoneNumber").val();
+let emailContact = $("#email").val();
+let emailOption = $("#emailradio").val();
+let telephoneOption = $("#teleradio").val();
+let genderContact = $("#gender").val();
+let whatsapp = $("#whatsappsvg").val();
+let proyect = $("#proyecto").val();
+//pref email.tel
+//genero
+//whatsapp
+//proyect
 $.ajax({
   url: "https://formsubmit.co/ajax/galomeggiolarobul@gmail.com",
   method: "POST",
   data: {
-    name: `${$("#name").val()}`,
-    secondName: `${$("#secondName").val()}`,
-    phoneNumber: `${$("#phoneNumber").val()}`,
-    email: `${$("#email").val()}`,
-    email: `${$("#email").val()}`,
-    email: `${$("#email").val()}`,
-    whatsapp: `${$("#whatsappsvg").val()}`,
-
-    message: `${$("#nombre").val()}`,
+    name: `${nameContact}`,
+    secondName: `${secondNameContact}`,
+    phoneNumber: `${phoneNumberContact}`,
+    email: `${emailContact}`,
+    telOrEmail: `${emailOption}, ${telephoneOption}`,
+    gender: `${genderContact}`,
+    whatsapp: `${whatsapp}`,
+    message: `${proyect}`,
   },
   dataType: "json",
 });
