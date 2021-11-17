5b3b7053ff62515068238e86632c92d4867cce93
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-17 02:05:35 -0300

    remake form

--- js/form.js
+++ js/form.js
@@ -1,28 +1,19 @@
-// let nameContact = ;
-// let secondNameContact = ;
-// let phoneNumberContact = ;
-// let emailContact = ;
-// let emailOption = ;
-// let telephoneOption = ;
-// let genderContact =;
-// let whatsapp = ;
-// let proyect = ;
-
-$.ajax({
-  method: "POST",
-  url: "https://formsubmit.co/ajax/galomeggiolarobul@gmail.com",
-  dataType: "json",
-  accepts: "application/json",
-  data: {
-    name: $("#name").val(),
-    secondName: $("#secondName").val(),
-    phoneNumber: $("#phoneNumber").val(),
-    email: $("#email").val(),
-    telOrEmail: [$("#emailradio").val(), $("#teleradio").val()],
-    gender: $("#gender").val(),
-    whatsapp: $("#whatsappsvg").val(),
-    message: $("#proyecto").val(),
-  },
-  success: (data) => console.log(data),
-  error: (err) => console.log(err),
+$(function(){
+  $("#contactForm").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: href,
+          data: $(this).serialize(),
+          success: function(response){
+              if(response.status == "success"){
+                  alert("We received your submission, thank you!");
+              }else{
+                  alert("An error occured: " + response.message);
+              }
+          }
+      });
+  });
 });
