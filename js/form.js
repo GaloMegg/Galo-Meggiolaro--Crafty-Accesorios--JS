09a4d0aacab44815c7f99a1fde18b61db1cee5a5
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-17 02:42:20 -0300

    whoarewe form

--- js/form.js
+++ js/form.js
@@ -37,3 +37,23 @@ $(function(){
       });
   });
 });
+
+$(function(){
+  $("#letUsContact").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: href,
+          data: $(this).serialize(),
+          success: function(response){
+              if(response.status == "success"){
+                  alert("¡Te esperamos!");
+              }else{
+                  alert("Uy, ¡algo malo paso! Intenta en otro momento..." + response.message);
+              }
+          }
+      });
+  });
+});
