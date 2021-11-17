76dd0d3df514ec6d84e11a39491ae1be09e11936
WtsCrackin <galomeggiolarobul@gmail.com>
2021-11-16 23:09:14 -0300

    form changes

--- js/form.js
+++ js/form.js
@@ -2,8 +2,15 @@ $.ajax({
   url: "https://formsubmit.co/ajax/galomeggiolarobul@gmail.com",
   method: "POST",
   data: {
-      name: "FormSubmit",
-      message: "I'm from Devro LABS"
+    name: `${$("#name").val()}`,
+    secondName: `${$("#secondName").val()}`,
+    phoneNumber: `${$("#phoneNumber").val()}`,
+    email: `${$("#email").val()}`,
+    email: `${$("#email").val()}`,
+    email: `${$("#email").val()}`,
+    whatsapp: `${$("#whatsappsvg").val()}`,
+
+    message: `${$("#nombre").val()}`,
   },
-  dataType: "json"
+  dataType: "json",
 });
